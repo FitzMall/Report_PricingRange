c9c1e1743243e18b4dc9c33a5539414fc4fbe6e9
BurroughsD <92319508+BurroughsD@users.noreply.github.com>
2022-11-04 09:40:14 -0400

    vehicle drill down deleted and added again as a different form

--- Report_PricingRange/Controllers/ReportController.cs
+++ Report_PricingRange/Controllers/ReportController.cs
@@ -78,7 +78,7 @@ namespace Report_PricingRange.Controllers
         {
 
             
-            return Redirect("TemplateReport");
+            return Redirect("VehicleDrillDown.cshtml");
 
         }
     }
